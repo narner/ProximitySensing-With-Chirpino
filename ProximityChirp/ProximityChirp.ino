0ca59ffc98cc1b3a5c6f23ef04d14e7b00f33701
Nicholas Arner <nicholasarner@gmail.com>
2015-08-14 10:47:32 -0400

    Proximity sensor setup.

--- ProximityChirp/ProximityChirp.ino
+++ ProximityChirp/ProximityChirp.ino
@@ -4,12 +4,21 @@
 // Change Beak to PortamentoBeak for a different sound
 Beak beak;
 
+int sensorPin = 0; //analog pin 0
+
 
 void setup() {
+    Serial.begin(9600);
 }
 
 
 void loop() {
-    beak.chirp("769jhvac9dm282qo58");
-    delay(2000);
+  beak.chirp("769jhvac9dm282qo58");
+//  delay(2000);
+    
+  int val = analogRead(sensorPin);
+  Serial.println(val);
+
+  //just to slow down the output - remove if trying to catch an object passing by
+  delay(100);
 }
