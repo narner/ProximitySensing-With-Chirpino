ffe70f55d1195d141a1d68540b76b27fbe51583f
Nicholas Arner <nicholasarner@gmail.com>
2015-08-14 11:39:55 -0400

    Chirp only when the proximity sensor reaches a certain threshold.

--- ProximityChirp/ProximityChirp.ino
+++ ProximityChirp/ProximityChirp.ino
@@ -1,24 +1,24 @@
 #include <ChirpinoSing.h>
 
-
 // Change Beak to PortamentoBeak for a different sound
 Beak beak;
 
-int sensorPin = 0; //analog pin 0
+// Analog pin 0 (proximity sensor input)
+int sensorPin = 0; 
 
 
 void setup() {
     Serial.begin(9600);
 }
 
-
 void loop() {
-  beak.chirp("769jhvac9dm282qo58");
-//  delay(2000);
     
   int val = analogRead(sensorPin);
   Serial.println(val);
 
-  //just to slow down the output - remove if trying to catch an object passing by
-  delay(100);
+  //Set a threshold for when to chirp. 
+  if (val > 200) {
+      beak.chirp("769jhvac9dm282qo58");
+      delay(2000);
+  }
 }
