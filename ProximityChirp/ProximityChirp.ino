c74aa505ec97bced54d3a0b385c5149fa8c8268b
Nicholas Arner <nicholasarner@gmail.com>
2015-08-14 10:38:37 -0400

    Porting example code from the Chirpino repo for test.

---
+++ ProximityChirp/ProximityChirp.ino
@@ -0,0 +1,15 @@
+#include <ChirpinoSing.h>
+
+
+// Change Beak to PortamentoBeak for a different sound
+Beak beak;
+
+
+void setup() {
+}
+
+
+void loop() {
+    beak.chirp("769jhvac9dm282qo58");
+    delay(2000);
+}
